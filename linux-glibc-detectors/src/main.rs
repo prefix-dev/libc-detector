359dae8c006e61c35969aecdd1df0f2cc19d751e
Bas Zalmstra <bas@prefix.dev>
2024-07-14 22:30:36 +0200

    workflow

--- linux-glibc-detectors/src/main.rs
+++ linux-glibc-detectors/src/main.rs
@@ -1,14 +1,16 @@
 #![no_std]
 #![no_main]
 
+// Link with libc
+#[link(name = "c")]
+extern "C" {}
+
 extern crate libc;
 
 #[no_mangle]
 pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    // Since we are passing a C string the final null character is mandatory.
-    const HELLO: &'static str = "Hello, world!\n\0";
     unsafe {
-        libc::printf(HELLO.as_ptr() as *const _);
+        libc::puts(libc::gnu_get_libc_version());
     }
     0
 }
